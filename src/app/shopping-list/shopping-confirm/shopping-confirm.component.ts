1b90fe9c361e4681dbddfe95637e3b31bfd0e331
LeonidasJD <milandjordjevic633@gmail.com>
2022-12-30 14:18:19 +0100

    made method for intstance ingredients array

--- src/app/shopping-list/shopping-confirm/shopping-confirm.component.ts
+++ src/app/shopping-list/shopping-confirm/shopping-confirm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient-model';
+import { ShoppingServiceService } from '../shopping-service.service';
 
 @Component({
   selector: 'app-shopping-confirm',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingConfirmComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shopingService:ShoppingServiceService) { }
+
+  ingredients: Ingredient[];
 
   ngOnInit(): void {
+    this.ingredients = this.shopingService.getIngredients();
   }
 
 }
