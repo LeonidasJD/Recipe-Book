fe8e34685c1376c8f9d0658ab57c25392b06484b
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-01-12 13:49:35 +0100

    added new value in recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,18 +2,22 @@ import { Ingredient } from "../shared/ingredient-model";
 
 export class Recipe{
 
-    
+
     public name: string;
     public description: string;
     public imagePath: string;
     public ingredients:Ingredient[];
+    public howToPrepare:string;
+    public recipeType:string;
 
-    constructor(name: string, description: string, imagePath: string,ingredients:Ingredient[]){
-        
+    constructor(name: string, description: string, imagePath: string,ingredients:Ingredient[],howToPrepare:string,recipeType:string){
+
         this.name = name;
         this.description = description;
-        this.imagePath = imagePath; 
+        this.imagePath = imagePath;
         this.ingredients = ingredients;
+        this.howToPrepare = howToPrepare;
+        this.recipeType = recipeType;
 
     }
 }
